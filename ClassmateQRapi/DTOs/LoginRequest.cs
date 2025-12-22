a4a5cd14530152268183311f6e92130f68404738
Xuan Duc <xuanducdong2004@gmail.com>
2025-12-22 13:48:27 +0700

    Tạo DTO cho chức năng đăng nhập, đăng ký

---
+++ ClassmateQRapi/DTOs/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ClassmateQRapi.DTOs
+{
+    public class LoginRequest
+    {
+        public string UserNameOrEmail { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ ClassmateQRapi/DTOs/RegisterRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClassmateQRapi.DTOs
+{
+    public class RegisterRequest
+    {
+        [Required]
+        public string UserName { get; set; } = null!;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = null!;
+
+
+
+        // File avatar (có thể null)
+        public IFormFile? Avatar { get; set; }
+    }
+}
