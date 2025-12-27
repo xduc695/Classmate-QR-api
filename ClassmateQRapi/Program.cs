f0c61eaac2d59d115e2a9c2f4f409bb7e9e1eb7c
Xuan Duc <xuanducdong2004@gmail.com>
2025-12-27 14:07:02 +0700

    cập nhật lại AttendanceController, thêm cơ sỡ dữ liệu và dtos và uploads các file tài liệu

--- ClassmateQRapi/Program.cs
+++ ClassmateQRapi/Program.cs
@@ -158,7 +158,7 @@ if (app.Environment.IsDevelopment())
 var avatarPath = Path.Combine(builder.Environment.ContentRootPath, "Avatars");
 var assignmentsPath = Path.Combine(builder.Environment.ContentRootPath, "assignments");
 var submissionsPath = Path.Combine(builder.Environment.ContentRootPath, "Submissions");
-
+var courseResourcesPath = Path.Combine(builder.Environment.ContentRootPath, "CourseResources");
 // Đảm bảo thư mục tồn tại
 Directory.CreateDirectory(avatarPath);
 Directory.CreateDirectory(assignmentsPath);
@@ -181,7 +181,18 @@ app.UseStaticFiles(new StaticFileOptions
     FileProvider = new PhysicalFileProvider(submissionsPath),
     RequestPath = "/Submissions"
 });
-
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(courseResourcesPath),
+    RequestPath = "/courseresources"
+});
+// Mở quyền truy cập thư mục CourseMaterials
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "CourseMaterials")),
+    RequestPath = "/coursematerials"
+});
 // 9. Rate limiter
 app.UseRateLimiter();
 
